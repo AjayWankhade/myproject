c1e69dc050c82ed81d10dbbca2c5f4756fbbb647
AJAY <98700861+AjayWankhade@users.noreply.github.com>
2023-01-10 17:33:20 +0530

    Changes done in App.file

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-import ResetPassword from './components/ResetPassword';
-import Wallet from './components/Wallet';
-import Array from './components/Array';
-import filter from './filter.png';
-import clock from './clock.png';
-import Toggle from './components/Toggle';
-import BotDealsUI from './components/BotDealsUI';
-import Bot_Deals from './components/Bot_Deals';
-import FormValidation from './components/FormValidation';
-import Binance_Futures from './components/Binance_Futures';
+import { useState } from 'react';
+import { QueryClientProvider, QueryClient } from 'react-query';
+//react Routing import
+import Planet from './Component/Planet';
+import ModelSuccessful from './Component/ModelSuccessful';
+
+
+export const queryClient = new QueryClient();
 
 function App() {
-  return (
-     <div className="App" 
-        style={{width:'100%',height:'1200px',backgroundColor:'lightgrey'}} >
-             
-             <Binance_Futures></Binance_Futures>
-            
-   </div>
-   /* <Toggle></Toggle>
 
-   <BotDealsUI></BotDealsUI>
-   < Bot_Deals></Bot_Deals>
-    
-   < */
-  //  
-  // <FormValidation></FormValidation>
-  // {/* <ResetPassword></ResetPassword>  */}
+  
+  return (
+
+    <QueryClientProvider client={queryClient} >
+    <div className="App" >
+  
+     
+{/* <Planet></Planet> */}
+
+<ModelSuccessful></ModelSuccessful>
+    </div>
+    </QueryClientProvider>
+
   );
+  
 }
 
 export default App;
