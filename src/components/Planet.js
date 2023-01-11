8c6fe8778893bac46fed502ae7da3eff301e4fe6
AJAY <98700861+AjayWankhade@users.noreply.github.com>
2023-01-11 19:27:31 +0530

    Task 7 Added

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -1,88 +1,93 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from 'react-query'
 
 const Planet = () => {
+  const Data1 = { display:'flex',justifyContent:'center',fontWeight:'bold', alignItems:'center', height:'60px',width:'100%',border:'0px solid'}
+const Data2 = {height:'20px',width:'100%', display:'flex',justifyContent:'center',alignItems:'center',border:'0px solid'}
 
-  //Fetching data from Api endpoint by reactQuery through Axios library
-    const {error,isLoading,data} = useQuery('information',()=>{ 
-        return axios.get('https://swapi.dev/api/planets/');
-    });
+const {isLoading, error, data} = useQuery('data', async() =>{
+  return await axios
+      .get ("http://swapi.dev/api/planets/")
+      .then((res) => (res.data.results))
+ })
 
+if (isLoading) 
+{
+  return <div>Loading.....</div>
+}
 
+if(error) 
+{
+  return <div>error.....</div>
+}
+return (
 
-//     // //Fetching data from Api endpoint by reactQuery through Fetch library 
-// const {isLoading,Error,data}= useQuery('GetKeyData',()=>{
-//   fetch(' http://swapi.dev/api/planets/').then(res=>
-//   res.json()
-//   )
-
-// })
-
-// if(isLoading)
-// {
-//   return <div>Loading...</div>
-// }
-// else if(Error)
-// {
-//   return <div>Error...</div>
-// }
-
-
-
-
-    //Call isLoading Property of query
-    if(isLoading){
-        return <h1>Loading...</h1>
-    }
-    if(error)
-    {
-        return <h1>Error Comes</h1>
-    }
-//console.log(data.data.results);
-  return (
-    <div style={{width:'100%'}}>
-
-      <h1 >Planets Data</h1>
-      <h1 style={{color:'blue'}}>Fetch Data From EndPoint</h1>
-
-
-      <div className='information' style={{display:'flex',justifyContent:'center', alignItems:'center',width:'100%'}}>
-        <table style={{alignSelf:'space-between', width:'100%',border:'2px solid grey'}} >
-            <tr >
-                <th>Name</th>
-                <th>Orbital Period</th>
-                <th>Diameter</th>
-                <th>Climate</th>
-                <th>Population</th>
-            </tr>
-      {
-
-        //Map Data one by one n
-        data.data.results.map((data,index)=>{
-           
-            const {name, orbital_period, diameter, climate,population} = data
-            return (
-              
-                <tr key={index}>
-                    <td>{name}</td>
-                    <td>{orbital_period}</td>
-                    <td>{diameter}</td>
-                    <td>{climate}</td>
-                    <td>{population}</td>
-                  
-                </tr>
-               
-            
-              
-            )
-         })
-      }
-      </table>
+  <div style={{ padding:'20px'}}>
+    <h1 >Planets Data</h1>
+      <h1 style={{color:'blue'}}>Fetch Data From API EndPoint</h1>
+  <div style={{display:'flex',flexDirection:'row',justifyContent:'space-between',gap:'10px'}}>
+      <div style={Data1}>
+          Name
       </div>
 
-    </div>
-  )
+      <div style={Data1}>
+      orbital_period
+      </div>
+
+      <div style={Data1}>
+          Rotation period
+      </div>
+
+      <div style={Data1}>
+      climate
+      </div>
+
+    
+  </div>
+
+
+
+  <div  >
+       
+       {data.map((ele) => {
+         return (
+           <div key={ele.name} 
+               style={{marginTop:'40px',display:'flex',flexDirection:'row', justifyContent:'space-between', gap:'30px'}} >
+
+             <div style={Data2}>
+                    {ele.name}
+               </div>
+
+             <div style={Data2}>
+                   {ele.orbital_period}
+               </div>
+
+             <div style={Data2}>
+                   {ele.rotation_period}
+               </div>
+
+             <div style={Data2}>
+                       {ele.climate}
+               </div>
+           </div>
+           
+         )
+       })}
+
+   </div>
+   
+   </div>
+)
+ 
+
+
+
+  
+
+ 
+
+
 }
 
 export default Planet;
