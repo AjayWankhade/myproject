e14d3538b34a16b15e9e403c8020283426d1f010
AJAY <98700861+AjayWankhade@users.noreply.github.com>
2023-01-11 09:24:02 +0530

    Changes updated

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -4,28 +4,50 @@ import { useQuery } from 'react-query'
 
 const Planet = () => {
 
-  //Fetching data from Api endpoint by reactQuery through Axios 
-    const query = useQuery('info',()=>{ 
+  //Fetching data from Api endpoint by reactQuery through Axios library
+    const {error,isLoading,data} = useQuery('information',()=>{ 
         return axios.get('https://swapi.dev/api/planets/');
     });
 
 
+
+//     // //Fetching data from Api endpoint by reactQuery through Fetch library 
+// const {isLoading,Error,data}= useQuery('GetKeyData',()=>{
+//   fetch(' http://swapi.dev/api/planets/').then(res=>
+//   res.json()
+//   )
+
+// })
+
+// if(isLoading)
+// {
+//   return <div>Loading...</div>
+// }
+// else if(Error)
+// {
+//   return <div>Error...</div>
+// }
+
+
+
+
     //Call isLoading Property of query
-    if(query.isLoading){
+    if(isLoading){
         return <h1>Loading...</h1>
     }
-    if(query.isError)
+    if(error)
     {
         return <h1>Error Comes</h1>
     }
-//console.log(query.data.data.results);
+//console.log(data.data.results);
   return (
     <div style={{width:'100%'}}>
 
       <h1 >Planets Data</h1>
+      <h1 style={{color:'blue'}}>Fetch Data From EndPoint</h1>
 
 
-      <div className='info' style={{display:'flex',justifyContent:'center', alignItems:'center',width:'100%'}}>
+      <div className='information' style={{display:'flex',justifyContent:'center', alignItems:'center',width:'100%'}}>
         <table style={{alignSelf:'space-between', width:'100%',border:'2px solid grey'}} >
             <tr >
                 <th>Name</th>
@@ -37,7 +59,7 @@ const Planet = () => {
       {
 
         //Map Data one by one n
-        query.data.data.results.map((data,index,arr)=>{
+        data.data.results.map((data,index)=>{
            
             const {name, orbital_period, diameter, climate,population} = data
             return (
@@ -63,4 +85,4 @@ const Planet = () => {
   )
 }
 
-export default Planet
+export default Planet;
