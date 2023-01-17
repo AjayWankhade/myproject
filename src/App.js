8462d65cb45a46108e81cd4bf9632099165fac4f
AJAY <98700861+AjayWankhade@users.noreply.github.com>
2023-01-17 11:21:47 +0530

    Updated app .js For Full Stack Project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,65 @@
+// import './App.css';
+// import { useState } from 'react';
+// import { QueryClientProvider, QueryClient } from 'react-query';
+// //react Routing import
+// import Planet from './Component/Planet';
+// import ModelSuccessful from './Component/ModelSuccessful';
+// import FormValidation_ApiIntegration from './Component/FormValidation_ApiIntegration';
+
+
+// export const queryClient = new QueryClient();
+
+// function App() {
+
+  
+//   return (
+
+//     <QueryClientProvider client={queryClient} >
+//     <div className="App" >
+  
+     
+// {/* <Planet></Planet> */}
+// // FormValidation_ApiIntegration
+
+// <ModelSuccessful></ModelSuccessful>
+//     </div>
+//     </QueryClientProvider>
+
+//   );
+  
+// }
+
+// export default App;
+
+
+import React from 'react'
 import './App.css';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { QueryClientProvider, QueryClient } from 'react-query';
 //react Routing import
 import Planet from './Component/Planet';
 import ModelSuccessful from './Component/ModelSuccessful';
 import FormValidation_ApiIntegration from './Component/FormValidation_ApiIntegration';
+import axios from 'axios';
+import {Routes, Route} from 'react-router-dom';
+import Registration_11 from './Component/Registration_11';
+import Task_10route from './Component/Task_10route';
 
 
-export const queryClient = new QueryClient();
+ const queryClient = new QueryClient();
 
 function App() {
 
-  
   return (
 
     <QueryClientProvider client={queryClient} >
     <div className="App" >
-  
+    {/* <Planet></Planet>  */}
      
-{/* <Planet></Planet> */}
-// FormValidation_ApiIntegration
-
-<ModelSuccessful></ModelSuccessful>
+       <Task_10route></Task_10route>
+       <Registration_11></Registration_11>
+   {/* <FormValidation_ApiIntegration></FormValidation_ApiIntegration>  */}
+{/* <ModelSuccessful></ModelSuccessful> */}
     </div>
     </QueryClientProvider>
 
@@ -30,3 +68,6 @@ function App() {
 }
 
 export default App;
+
+
+
